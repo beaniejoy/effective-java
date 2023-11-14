e1cbf3ce7f5b5a26d736dea9da9196fb20090ab8
beaniejoy <lhb1025@gmail.com>
2023-11-15 02:34:15 +0900

    아이템1: 인터페이스 정적 팩토리 메소드 사용 정리까지

--- src/main/java/me/whiteship/chapter01/item01/HelloServiceFactory.java
+++ src/main/java/me/whiteship/chapter01/item01/HelloServiceFactory.java
@@ -10,12 +10,15 @@ import java.util.ServiceLoader;
 public class HelloServiceFactory {
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        // HelloService interface of 메소드에 구체적인 클래스의 객체 생성할 필요가 없다.
+        // 등록 >> chinese hello project에서 resources/META-INF/services/...[package 경로].../ChineseHelloService
         ServiceLoader<HelloService> loader = ServiceLoader.load(HelloService.class);
         Optional<HelloService> helloServiceOptional = loader.findFirst();
         helloServiceOptional.ifPresent(h -> {
             System.out.println(h.hello());
         });
 
+        // 아래와 같이 HelloService를 생성해도 되나 ChineseHelloService에 의존적이게 된다.(import 해야 함)
         HelloService helloService = new ChineseHelloService();
         System.out.println(helloService.hello());
 
